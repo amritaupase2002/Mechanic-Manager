f86bcd0199dff1e7f22f410a975d975b91dbe7fb
amritaupase2002 <amritaupase10@gmail.com>
2025-07-07 16:35:53 +0530

    url Changed

--- frontend/src/utils/Constant.jsx
+++ frontend/src/utils/Constant.jsx
@@ -1,3 +1,3 @@
-export const LOCAL_URL = "https://mechanic-backend.syolosoft.com";
+export const LOCAL_URL = "https://mechanic-manager-one.vercel.app";
 // export const LOCAL_URL = "http://192.168.1.41:5000";
  
